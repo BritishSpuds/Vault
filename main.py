e3184c3e0c316df93c14a25799526ce52eb51f33
BritishSpuds <84645480+BritishSpuds@users.noreply.github.com>
2022-04-27 18:14:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,42 @@
 import tkinter as tk
-import youtube_dl as ydl
+from tkinter import filedialog as fd
+import youtube_dl
+import moviepy as mp
 
 
 def start():
-    urlget = str(urlentry.get())
-    formatget = str(selected.get())
-    rateget = int(rateselected.get())
-    print("Selected URL: ", urlget)
-    print("Selected Format: ", formatget)
-    print("Selected Rate Limit: ", rateget, "Bytes/s")
-    ydl_opts = {
-        'outtmpl': 'c:/tmp/%(title)s.%(ext)s'--restrict-filenames,
-        'format': formatget,
-        'ratelimit': rateget,
-    }
-    with ydl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([urlget])
+    urlget = urlentry.get()
+    if urlget == "":
+        print("Not a valid URL")
+        pass
+    else:
+        urlget = str(urlentry.get())
+        formatget = str(selected.get())
+        rateget = str(rateselected.get())
+        print("Selected URL: ", urlget)
+        print("Selected Format: ", formatget)
+        print("Selected Rate Limit: ", rateget, "Bytes/s")
+        ydl_opts = {
+            'outtmpl': 'c:/tmp/%(title)s.%(ext)s',
+            'format': formatget,
+            'ratelimit': rateget,
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([urlget])
 
+def callback():
+    file = fd.askopenfilename()
+    print("Selected File:",file)
+    clip = mp.VideoFileClip(str(file))
+    clip.audio.write_audiofile(file)
 
 mainwindow = tk.Tk()
 mainwindow.geometry("475x330")
 mainwindow.resizable(0, 0)
 mainwindow.title("Jake's Youtube Downloader")
-# mainwindow.iconbitmap("icon.ico")  # BROKEN WHEN CONVERTED TO EXE FOR SOME REASON
+# mainwindow.iconbitmap("icon.ico")   # BROKEN WHEN CONVERTED TO EXE FOR SOME REASON
 
-downloadlocationwindow = tk.Tk()
-downloadlocationwindow.geometry("120x90")
-downloadlocationwindow.resizable(0, 0)
-downloadlocationwindow.title("Jake's Youtube Downloader")
-
-
-options = [
+formats = [
     "mp4",
     "webm",
     "mkv",
@@ -56,29 +62,46 @@ rateoptions = [
     "1000000",
 ]
 
-selected = tk.StringVar(mainwindow)
-selected.set(options[0])
 
-downloaderlabelframe = tk.LabelFrame(mainwindow, text="Downloader", bg="grey94", width=465, height=90)
-converterlabelframe = tk.LabelFrame(mainwindow, text="Converter", bg="grey94", width=250, height=230)
-wiplabel = tk.Label(mainwindow, text="This Section Is Currently Work In Progress.")
-brokenlabel = tk.Label(mainwindow, text="Format Selection Is Currently Broken. (mp4 Only)")
-pluglabel = tk.Label(mainwindow, text="I Have A Twitch Channel\n And Dontation Link At\n linktr.ee/britishspuds")
+selected = tk.StringVar(mainwindow)
+selected.set("Format")
+
+converterselected = tk.StringVar(mainwindow)
+converterselected.set("Format")
+
+rateselected = tk.StringVar(mainwindow)
+rateselected.set("Ratelimit")
+
+# Downloader GUI
+
+downloaderlabelframe = tk.LabelFrame(mainwindow, text="Downloader", bg="grey94", width=465, height=140)
 urllabel = tk.Label(mainwindow, fg="black", text="Url:")
 urlentry = tk.Entry(mainwindow, fg="black", bg="white", width=50)
-startbutton = tk.Button(mainwindow, text="Start", width=10, height=3, bg="white", fg="black", command=start)
-formatselection = tk.OptionMenu(mainwindow, selected, *options, )
-ratelimitselection = tk.OptionMenu(mainwindow, rateselected, *rateoptions, )
+startbutton = tk.Button(mainwindow, text="Start", width=10, height=3, bg="grey94", fg="black", command=start)
+formatselection = tk.OptionMenu(mainwindow, selected, *formats,)
+rateselection = tk.OptionMenu(mainwindow, rateselected, *rateoptions,)
+
+# Converter GUI
+
+converterlabelframe = tk.LabelFrame(mainwindow, text="Converter", bg="grey94", width=250, height=180)
+openfilebutton = tk.Button(mainwindow, text="select file", bg="grey94", fg="black",width=10, height=2, command=callback)
+converterformatselection = tk.OptionMenu(mainwindow, converterselected, *formats,)
+
+
+# Other GUI
+
+pluglabel = tk.Label(mainwindow, text="I Have A Twitch Channel And\n Dontation Link At linktr.ee/britishspuds")
 
 downloaderlabelframe.place(x=5, y=5)
-converterlabelframe.place(x=5, y=95)
-wiplabel.place(x=15, y=110)
-brokenlabel.place(x=90, y=60)
-pluglabel.place(x=290, y=110)
+converterlabelframe.place(x=5, y=145)
+pluglabel.place(x=260, y=155)
 urllabel.place(x=15, y=25)
 urlentry.place(x=50, y=25)
 startbutton.place(x=375, y=25)
 formatselection.place(x=15, y=55)
-ratelimitselection.place(x=15, y=105)
+rateselection.place(x=100, y=55)
+openfilebutton.place(x=15, y=165)
+converterformatselection.place(x=13, y=210)
+
 
 mainwindow.mainloop()
