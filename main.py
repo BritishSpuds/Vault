0e1ec389fbe00203c97c63105494ef01a0369141
Jake Thornley <84645480+BritishSpuds@users.noreply.github.com>
2022-05-02 23:27:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import tkinter as tk
-from tkinter import filedialog as fd
+from tkinter import filedialog
+from tkinter import ttk
 import youtube_dl
 
 
-def start():
+def startdownloader():
     urlget = urlentry.get()
     if urlget == "":
         print("Not a valid URL")
@@ -40,36 +41,31 @@ def start():
 
 
 def callback():
-    file = str(fd.askopenfilename())
+    file = str(filedialog.askopenfilename())
     print("Selected File:", file)
     directoryentry.delete(0, "end")
     directoryentry.insert(string=file, index=0)
 
 
-def formathelpwindow():
-    tkhelpwindow = tk.Tk()
-    tkhelpwindow.geometry("375x140")
-    tkhelpwindow.resizable(0, 0)
-    tkhelpwindow.title("Help")
-    tkhelpwindow.iconbitmap("icon.ico")   # BROKEN WHEN CONVERTED TO EXE FOR SOME REASON
-
-    downloaderhelplabelframe = tk.LabelFrame(tkhelpwindow, text="Formats",  bg="grey94", width=365, height=130)
-
-    downloaderhelp = tk.Label(tkhelpwindow, text="Format is the extention on the end of the file. This converter\n"
-                                                 "supports: Mp4, Webp and M4a. Mp4 can be used if you want your\n"
-                                                 "file to be downloaded with video and audio included. Webp can\n"
-                                                 "be used if you want your file to be downloaded with video but\n"
-                                                 "without audio. M4a can be used if you just want to download\n"
-                                                 "your file with audio. Any can be used.\n")
-    downloaderhelp.place(x=10, y=20)
-    downloaderhelplabelframe.place(x=5, y=5)
+def startconverter():
+    pass
 
 
 mainwindow = tk.Tk()
 mainwindow.geometry("475x330")
 mainwindow.resizable(0, 0)
 mainwindow.title("Jake's Youtube Downloader")
-mainwindow.iconbitmap("icon.ico")   # BROKEN WHEN CONVERTED TO EXE FOR SOME REASON
+# mainwindow.iconbitmap("icon.ico")   # BROKEN WHEN CONVERTED TO EXE FOR SOME REASON
+tabControl = ttk.Notebook(mainwindow)
+
+DownloaderTab = ttk.Frame(tabControl)
+ConverterTab = ttk.Frame(tabControl)
+HelpTab = ttk.Frame(tabControl)
+
+tabControl.add(DownloaderTab, text="Downloader")
+tabControl.add(ConverterTab, text="Converter")
+tabControl.add(HelpTab, text="Help")
+tabControl.pack(expand=1, fill="both")
 
 
 formats = [
@@ -99,47 +95,43 @@ rateselected.set(rateoptions[4])
 
 # - MAIN WINDOW - #
 
-# Downloader GUI
+# Downloader Tab
 
-downloaderlabelframe = tk.LabelFrame(mainwindow, text="Downloader", bg="grey94", width=465, height=140)
-urllabel = tk.Label(mainwindow, fg="black", text="Url:")
-urlentry = tk.Entry(mainwindow, fg="black", bg="white", width=50)
-startbutton = tk.Button(mainwindow, text="Start", width=10, height=3, bg="grey94", fg="black", command=start)
-helpbutton = tk.Button(mainwindow, text="Help", width=8, height=1, bg="grey94", fg="black", command=formathelpwindow)
-formatselection = tk.OptionMenu(mainwindow, selected, *formats,)
-rateselection = tk.OptionMenu(mainwindow, rateselected, *rateoptions,)
+downloaderlabelframe = tk.LabelFrame(DownloaderTab, text="Downloader", bg="grey94", width=460, height=300)
+urllabel = tk.Label(DownloaderTab, fg="black", text="Url:")
+urlentry = tk.Entry(DownloaderTab, fg="black", bg="white", width=50)
+startdownloader = tk.Button(DownloaderTab, text="Start\nDownload", width=10, height=3, bg="grey94", fg="black", command=startdownloader)
+formatselection = tk.OptionMenu(DownloaderTab, selected, *formats,)
+rateselection = tk.OptionMenu(DownloaderTab, rateselected, *rateoptions)
 
-# Converter GUI
+# Converter Tab
 
-converterlabelframe = tk.LabelFrame(mainwindow, text="Converter", bg="grey94", width=250, height=180)
-openfilebutton = tk.Button(mainwindow, text="select file", bg="grey94", fg="black", width=10, height=2,
-                           command=callback)
-converterformatselection = tk.OptionMenu(mainwindow, converterselected, *formats,)
-directoryentry = tk.Entry(mainwindow, fg="black", bg="white", width=22)
+converterlabelframe = tk.LabelFrame(ConverterTab, text="Converter", bg="grey94", width=460, height=300)
+openfilebutton = tk.Button(ConverterTab, text="select file", bg="grey94", fg="black", width=10, height=2, command=callback)
+converterformatselection = tk.OptionMenu(ConverterTab, converterselected, *formats)
+directoryentry = tk.Entry(ConverterTab, fg="black", bg="white", width=46)
+directorylabel = tk.Label(ConverterTab, fg="black", bg="grey94", text="Directory:")
+startconverter = tk.Button(ConverterTab, text="Start\nConverter", width=10, height=3, bg="grey94", fg="black", command=startconverter)
 
-# Other GUI
+# Help Tab
 
-pluglabel = tk.Label(mainwindow, text="I Have A Twitch Channel And\n"
-                                      "Dontation Link At\n"
-                                      "linktr.ee/britishspuds\n"
-                                      "if you're feeling generous.")
+helplabelframe = tk.LabelFrame(HelpTab, text="Help", bg="grey94", width=460, height=300)
+helplabel = tk.Label(HelpTab, fg="black", bg="grey94", text="I will eventually put some helpful text here :)")
 
-# - HELP WINDOW - #
-
-# Downloader Help
-
-downloaderlabelframe.place(x=5, y=5)
-converterlabelframe.place(x=5, y=145)
-pluglabel.place(x=280, y=155)
+downloaderlabelframe.place(x=5, y=0)
+converterlabelframe.place(x=5, y=0)
+helplabelframe.place(x=5, y=0)
 urllabel.place(x=15, y=25)
 urlentry.place(x=50, y=25)
-startbutton.place(x=375, y=25)
-helpbutton.place(x=15, y=95)
+startdownloader.place(x=370, y=25)
 formatselection.place(x=15, y=55)
 rateselection.place(x=100, y=55)
-openfilebutton.place(x=15, y=165)
-converterformatselection.place(x=13, y=210)
-directoryentry.place(x=105, y=165)
+openfilebutton.place(x=15, y=60)
+converterformatselection.place(x=15, y=105)
+directoryentry.place(x=74, y=25)
+directorylabel.place(x=15, y=25)
+startconverter.place(x=370, y=25)
+helplabel.place(x=15, y=15)
 
 
 mainwindow.mainloop()
